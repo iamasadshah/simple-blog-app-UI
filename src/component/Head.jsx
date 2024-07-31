0a3fab3db35a7e0a3b059c14a6beebc7ca05dc00
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-07-31 12:10:15 +0500

    header modified

--- src/component/Head.jsx
+++ src/component/Head.jsx
@@ -3,43 +3,24 @@ import React from "react";
 const Head = () => {
   return (
     <>
-      <header className="p-3 text-bg-dark">
-        <div className="container">
-          <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <a
-              href="/"
-              className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
-            >
-              <svg
-                className="bi me-2"
-                width="40"
-                height="32"
-                role="img"
-                aria-label="Bootstrap"
-              >
-                <use xlinkHref="#bootstrap"></use>
-              </svg>
+      <header class="d-flex flex-wrap bg-dark  justify-content-center py-3 mb-4 border-bottom">
+        <ul class="nav nav-pills ">
+          <li class="nav-item">
+            <a href="#" class="nav-link text-white">
+              Home
             </a>
-
-            <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-              <li>
-                <a href="#" className="nav-link px-2 text-secondary">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="blogs" className="nav-link px-2 text-white">
-                  Blogs
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link px-2 text-white">
-                  About
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link text-white">
+              Blogs
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link text-white">
+              About
+            </a>
+          </li>
+        </ul>
       </header>
     </>
   );
--- src/component/Side.jsx
+++ src/component/Side.jsx
@@ -14,7 +14,7 @@ const Sidebar = ({ selectedTab, setSelectedTab }) => {
           <svg className="bi pe-none me-2" width="40" height="32">
             <use xlinkHref="#bootstrap"></use>
           </svg>
-          <span className="fs-4">Sidebar</span>
+          <span className="fs-4">BlogEase</span>
         </a>
         <hr />
         <ul className="nav nav-pills flex-column mb-auto">
