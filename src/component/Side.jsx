a1cdb55eeb3907d1afce8fa34f11eaca519f3f16
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-07-31 12:25:29 +0500

    name changed

--- src/component/Side.jsx
+++ src/component/Side.jsx
@@ -9,12 +9,12 @@ const Sidebar = ({ selectedTab, setSelectedTab }) => {
       >
         <a
           href="/"
-          className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
+          class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
         >
-          <svg className="bi pe-none me-2" width="40" height="32">
-            <use xlinkHref="#bootstrap"></use>
+          <svg class="bi me-2" width="40" height="32">
+            <use xlink:href="#bootstrap"></use>
           </svg>
-          <span className="fs-4">BlogEase</span>
+          <span class="fs-4"></span>
         </a>
         <hr />
         <ul className="nav nav-pills flex-column mb-auto">
