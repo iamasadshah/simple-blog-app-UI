38876c60ae437dd0236d80af24f67840d48f96ce
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-08-01 12:28:04 +0500

    new message component

--- src/component/Postlist.jsx
+++ src/component/Postlist.jsx
@@ -1,11 +1,14 @@
 import React, { useContext } from "react";
 import Post from "./Post";
 import { PostList as postListDate } from "../store/posts-list-store";
+import WelcomeMessage from "./WelcomeMessage";
+
 const Postlist = () => {
   const { postList } = useContext(postListDate);
   console.log(postList);
   return (
     <div id="blogs">
+      {postList.length === 0 && <WelcomeMessage />}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
---
+++ src/component/WelcomeMessage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const WelcomeMessage = () => {
+  return (
+    <div className="welcome-message">
+      <center>
+        <h1>There is currently no post</h1>
+      </center>
+    </div>
+  );
+};
+
+export default WelcomeMessage;
