f325dc080634f64e054b2622a8a6117646cada47
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-08-06 12:20:50 +0500

    about page completed

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const About = () => {
-  return <div>About us</div>;
+  return (
+    <div className="about-page">
+      <h1>About the Project</h1>
+      <p>
+        This is a blog post application built using React. It allows users to
+        create and view blog posts. The application employs a dummy JSON API to
+        retrieve and display blog post content.
+      </p>
+      <h2>About the Developer</h2>
+      <p>
+        I'm Asad Shah, a Web Developer. I created this project as a learning
+        experience to deepen my React JS proficiency.
+      </p>
+    </div>
+  );
 };
 
 export default About;
